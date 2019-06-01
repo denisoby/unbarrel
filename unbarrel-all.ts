b1ec8a14f72648c4d7a4cf4f87134c36b4782c69
denis.obydennykh <dobydennykh@griddynamics.com>
2019-06-01 17:18:28 +0300

    moved code to importVisitor

--- unbarrel-all.ts
+++ unbarrel-all.ts
@@ -17,65 +17,9 @@ function unbarrelAll(fileOrDirectory: string) {
     //     return false;
     // });
     project.getSourceFile(exportsVisitor);
-    // project.saveSync();
-
-    // project.getSourceFile(importsVisitor);
-
-    const importIndex: SourceFile = project.getSourceFileOrThrow('/Users/dobydennykh/dev/unbarrel/tests/importing/index.ts');
-
     delimiter();
-
-    const declarationsToBarrels = importIndex.getImportDeclarations().filter((importDeclaration: ImportDeclaration) => {
-
-        const moduleSpecifierSource = importDeclaration.getModuleSpecifierSourceFile()
-
-
-        let moduleSpecifierSourcePath;
-
-        if (moduleSpecifierSource) {
-            moduleSpecifierSourcePath = moduleSpecifierSource && moduleSpecifierSource.getFilePath();
-        }
-        else {
-            const moduleValue = importDeclaration.getModuleSpecifierValue();
-            if (['.', '/'].indexOf(moduleValue[0]) > -1) {
-                moduleSpecifierSourcePath = path.resolve(importIndex.getDirectoryPath(), moduleValue);
-            }
-        }
-
-
-        console.log('IMPORTED: ', moduleSpecifierSourcePath, importDeclaration.getModuleSpecifierValue());
-
-
-        if (moduleSpecifierSourcePath && (barrelsMap[moduleSpecifierSourcePath + '.ts'] ||
-            barrelsMap[moduleSpecifierSourcePath + '/index.ts'])) {
-            console.log('THIS IS BARREL! :)');
-            return true;
-        }
-        return false;
-    });
-
-   if(declarationsToBarrels.length) {
-       declarationsToBarrels.forEach((declaration: ImportDeclaration) => {
-           console.log('declaration: ', declaration.getModuleSpecifierValue());
-           declaration.remove();
-       });
-       delimiter();
-       importIndex.fixMissingImports();
-       // importIndex.saveSync();
-   }
-
-    console.log(importIndex.getFullText());
-
-    // project.saveSync();
-
-    // project = new Project();
-    // project.addExistingSourceFiles(fileOrDirectory);
-    // const importIndexFixed: SourceFile = project.getSourceFileOrThrow('/Users/dobydennykh/dev/unbarrel/tests/importing/index.ts');
-    // importIndexFixed.fixMissingImports();
-    // importIndexFixed.saveSync();
-
-    // importIndex.fixMissingImports();
-    // importIndex.saveSync();
+    project.getSourceFile(importsVisitor);
+    project.saveSync();
 }
 
 
@@ -101,27 +45,55 @@ function exportsVisitor(sourceFile: SourceFile): boolean {
     return false;
 }
 
+function isBarrelImport(directoryPath: string) {
+    return (importDeclaration: ImportDeclaration) => {
+        const moduleSpecifierSource = importDeclaration.getModuleSpecifierSourceFile();
+
+        let moduleSpecifierSourcePath;
+
+        if (moduleSpecifierSource) {
+            // probably will never happen, because
+            // 1. undefined for barrels
+            // 2. now I'm removing them, because they are useless for barrels and may prevent further fixMissingImports
+            moduleSpecifierSourcePath = moduleSpecifierSource && moduleSpecifierSource.getFilePath();
+        } else {
+            const moduleValue = importDeclaration.getModuleSpecifierValue();
+            if (['.', '/'].indexOf(moduleValue[0]) > -1) {
+                moduleSpecifierSourcePath = path.resolve(directoryPath, moduleValue);
+            }
+        }
+
+
+        // console.log('IMPORTED: ', moduleSpecifierSourcePath, importDeclaration.getModuleSpecifierValue());
+
+
+        if (moduleSpecifierSourcePath && (barrelsMap[moduleSpecifierSourcePath + '.ts'] ||
+            barrelsMap[moduleSpecifierSourcePath + '/index.ts'])) {
+            // console.log('THIS IS BARREL! :)');
+            return true;
+        }
+        return false;
+    };
+}
+
 function importsVisitor(sourceFile: SourceFile): boolean {
-    console.log('----------------------------------------------------------------');
-    console.log('importsVisitor: ', sourceFile.getFilePath());
 
-    // const exportsDeclarations = sourceFile.organizeImports();
+    let importVerifier = isBarrelImport(sourceFile.getDirectoryPath());
+    const declarationsToBarrels = sourceFile.getImportDeclarations().filter(importVerifier);
 
-    // const exportsMap = exportsDeclarations.reduce((accum: {[path: string]: string[]}, exportItem) => {
-    //     const fullPath = exportItem.getSourceFile().getFilePath();
-    //     const relativeFilePath = path.relative(basePath, fullPath);
-    //
-    //     accum[relativeFilePath] = [];
-    //
-    //     return accum;
-    // }, {});
-    //
-    // console.log(exportsMap);
-    //
+    if(declarationsToBarrels.length) {
+        console.log('FOUND BARREL IMPORTS: ', sourceFile.getFilePath());
+        declarationsToBarrels.forEach((declaration: ImportDeclaration) => {
+            console.log('declaration: ', declaration.getModuleSpecifierValue());
+            declaration.remove();
+        });
+        // delimiter();
+        sourceFile.fixMissingImports();
+        // importIndex.saveSync();
+    }
+
     return false;
 }
 
 
 unbarrelAll('/Users/dobydennykh/dev/unbarrel/tests/**/*.ts');
-// unbarrelAll('/Users/dobydennykh/dev/unbarrel/tests/export-barrels/1-export-barrel.ts');
-// unbarrelAll('/Users/dobydennykh/dev/unbarrel/tests/importing/index.ts');
