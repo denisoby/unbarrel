5bca165510c8508fc14ec5750186475140deb59c
denis.obydennykh <dobydennykh@griddynamics.com>
2019-06-01 14:11:38 +0300

    import tests unbarrel - in progress

--- tests/importing/index.ts
+++ tests/importing/index.ts
@@ -1,5 +1,8 @@
 import { HELLO2 } from "../export-barrels";
+import * as X from "../export-barrels";
 import { HELLO_ONE } from "../export-barrels/nested-exports-with-index/1-A-nested-export";
+
+import * as lodash from "lodash";
 
 const hello1 = new HELLO_ONE();
 const hello2 = new HELLO2();
--- unbarrel-all.ts
+++ unbarrel-all.ts
@@ -1,12 +1,31 @@
 import {Project, SourceFile} from "ts-morph";
 import * as path from "path";
+import {log} from "util";
 
 function unbarrelAll(fileOrDirectory: string) {
     const project = new Project();
     project.addExistingSourceFiles(fileOrDirectory);
 
+    // project.getSourceFile(file => {
+    //     console.log(file.getFilePath());
+    //     return false;
+    // });
     // project.getSourceFile(exportsVisitor);
-    project.getSourceFile(importsVisitor);
+    // project.getSourceFile(importsVisitor);
+
+    const importIndex = project.getSourceFileOrThrow('/Users/dobydennykh/dev/unbarrel/tests/importing/index.ts');
+    const importLiterals = importIndex.getImportStringLiterals();
+
+    console.log(importLiterals);
+
+
+    let directory = importIndex.getDirectoryPath();
+    console.log(directory);
+    console.log(path.resolve(directory, '../some-path/file'));
+
+
+    // importIndex.fixMissingImports();
+    // importIndex.saveSync();
 }
 
 
@@ -37,21 +56,21 @@ function importsVisitor(sourceFile: SourceFile): boolean {
 
     const exportsDeclarations = sourceFile.organizeImports();
 
-    const exportsMap = exportsDeclarations.reduce((accum: {[path: string]: string[]}, exportItem) => {
-        const fullPath = exportItem.getSourceFile().getFilePath();
-        const relativeFilePath = path.relative(basePath, fullPath);
-
-        accum[relativeFilePath] = [];
-
-        return accum;
-    }, {});
-
-    console.log(exportsMap);
-
+    // const exportsMap = exportsDeclarations.reduce((accum: {[path: string]: string[]}, exportItem) => {
+    //     const fullPath = exportItem.getSourceFile().getFilePath();
+    //     const relativeFilePath = path.relative(basePath, fullPath);
+    //
+    //     accum[relativeFilePath] = [];
+    //
+    //     return accum;
+    // }, {});
+    //
+    // console.log(exportsMap);
+    //
     return false;
 }
 
 
-// unbarrelAll('/Users/dobydennykh/dev/unbarrel/tests/export-barrels/**/*.ts');
+unbarrelAll('/Users/dobydennykh/dev/unbarrel/tests/**/*.ts');
 // unbarrelAll('/Users/dobydennykh/dev/unbarrel/tests/export-barrels/1-export-barrel.ts');
-unbarrelAll('/Users/dobydennykh/dev/unbarrel/tests/importing/index.ts');
+// unbarrelAll('/Users/dobydennykh/dev/unbarrel/tests/importing/index.ts');
