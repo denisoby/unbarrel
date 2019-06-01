46651648e23a539d18d9565888b668224bf97010
denis.obydennykh <dobydennykh@griddynamics.com>
2019-06-01 15:39:47 +0300

    minor import fix

--- tests/importing/index.ts
+++ tests/importing/index.ts
@@ -1,5 +1,5 @@
 import { HELLO2 } from "../export-barrels";
-import * as X from "../export-barrels";
+import * as X from "../export-barrels/1-export-barrel";
 import { HELLO_ONE } from "../export-barrels/nested-exports-with-index/1-A-nested-export";
 
 import * as lodash from "lodash";
